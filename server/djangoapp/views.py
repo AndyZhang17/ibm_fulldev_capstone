cebff15034bfba7bdbd97d48546d857311a21e3e
AndyZhang17 <andytb17@gmail.com>
2024-07-31 11:48:43 +0800

    enable logout

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -39,9 +39,13 @@ def login_user(request):
         data["status"] = "Authenticated"
     return JsonResponse(data)
 
+
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    data = {"userName": ""}
+    return JsonResponse(data)
+
 
 # Create a `registration` view to handle sign up request
 # @csrf_exempt
